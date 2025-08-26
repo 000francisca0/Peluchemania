ec7c6c09d4cc65bf47556fc0103a00d1d12f69b2
000francisca0 <fr.sanhuezap@duocuc.cl>
2025-08-26 13:22:06 -0400

    Proyecto actualizado

---
+++ Peluchemania/funciones.js
@@ -0,0 +1,15 @@
+function allCaps(nombre, apellido){
+    return "Bienvenido a Peluchemania " + nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase() + " " + 
+    apellido.charAt(0).toUpperCase() + apellido.slice(1).toLowerCase();
+
+}
+
+function loadModalData(){
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    document.getElementById('modalEmail').textContent = email;
+    document.getElementById('modalSubject').textContent = subject;
+    document.getElementById('modalMessage').textContent = message;
+}
