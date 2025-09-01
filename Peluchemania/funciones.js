fcbf5c385a6ad7385c1b2a29c3c222409e6bd541
000francisca0 <fr.sanhuezap@duocuc.cl>
2025-09-01 17:28:04 -0400

    Actualizacion de load modal data

--- Peluchemania/funciones.js
+++ Peluchemania/funciones.js
@@ -5,21 +5,49 @@ function allCaps(nombre, apellido){
 }
 
 function loadModalData(){
+    // Datos del cliente 
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
 
-    document.getElementById('modalEmail').textContent = email;
-    document.getElementById('modalSubject').textContent = subject;
-    document.getElementById('modalMessage').textContent = message;
     
-    if(!email || !subject || !message){
-        alert("Completa todos los campos");
+    email.classList.remove('is-invalid', 'is-valid');
+    subject.classList.remove('is-invalid', 'is-valid');
+    message.classList.remove('is-invalid', 'is-valid');
+
+    let datosValidos = true; 
+
+    //validar email 
+    if(!email.value || !email.values.includes("@") || !email.values.includes(".")){
+        email.classList.add('is-invalid');
+        datosValidos=false; 
         return; 
     }
-    if(!email.includes("@") || !email.includes(".")){
-        alert("Porfavor ingresa un correo válido ");
+    //validar el asunto 
+    if(!subject.value){
+        subject.classList.add('is-invalid');
+        datosValidos = false; 
         return; 
     }
+    //validar el mensaje 
+    if(!message.value){
+        subject.classList.add('is-invalid');
+        datosValidos = false; 
+        return; 
+    }
+    if (!formularioValido) {
+        return;
+    }
+
+    
+    // 4. Si las validaciones pasan, carga los datos en el modal y añade la clase 'is-valid'
+    email.classList.add('is-valid');
+    subject.classList.add('is-valid');
+    message.classList.add('is-valid');
+
+    document.getElementById('modalEmail').textContent = email.value;
+    document.getElementById('modalSubject').textContent = subject.value;
+    document.getElementById('modalMessage').textContent = message.value;
+
       
 }
